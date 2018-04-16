6fc4445c08b5995e0073c9c2049d25b4689cbe35
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-04-16 15:53:36 +0200

    Fix scoring bug (#5)
    * Improve IoU method
    * More explicit boolean casting before saving

--- problem.py
+++ problem.py
@@ -18,11 +18,9 @@ class Predictions(MultiClassPredictions):
 
     def __init__(self, y_pred=None, y_true=None, n_samples=None):
         if y_pred is not None:
-            size = len(y_pred)
-            self.y_pred = np.reshape(y_pred, (size, self.n_columns))
+            self.y_pred = np.reshape(y_pred, (len(y_pred), self.n_columns))
         elif y_true is not None:
-            size = len(y_true)
-            y_true = np.reshape(y_true, (size, self.n_columns))
+            y_true = np.reshape(y_true, (len(y_true), self.n_columns))
             # self._init_from_pred_labels(y_true)
             self.y_pred = y_true
         elif n_samples is not None:
@@ -42,13 +40,50 @@ class Predictions(MultiClassPredictions):
 workflow = rw.workflows.ObjectDetector()
 
 
-def iou(y_true, y_pred):
+def iou_bitmap(y_true, y_pred, verbose=False):
+    """
+    Compute the IoU between two arrays
+
+    If the arrays are probabilities (floats) instead of predictions (integers
+    or booleans) they are automatically rounded to the nearest integer and
+    converted to bool before the IoU is computed.
+
+    Parameters
+    ----------
+    y_true : ndarray
+        array of true labels
+    y_pred : ndarray
+        array of predicted labels
+    verbose : bool (optional)
+        print the intersection and union separately
+
+    Returns
+    -------
+    float :
+        the intersection over union (IoU) value scaled between 0.0 and 1.0
+
+    """
     EPS = np.finfo(float).eps
-    y_true = y_true.astype(int)
-    y_pred = y_pred.astype(int)
-    intersection = np.sum(y_true * y_pred, axis=-1)
-    sum_ = np.sum(y_true + y_pred, axis=-1)
+
+    # Make sure each pixel was predicted e.g. turn probability into prediction
+    if y_true.dtype in [np.float32, np.float64]:
+        y_true = y_true.round().astype(bool)
+
+    if y_pred.dtype in [np.float32, np.float64]:
+        y_pred = y_pred.round().astype(bool)
+
+    # Reshape to 1d
+    y_true = y_true.ravel()
+    y_pred = y_pred.ravel()
+
+    # Compute intersection and union
+    intersection = np.sum(y_true * y_pred)
+    sum_ = np.sum(y_true + y_pred)
     jac = (intersection + EPS) / (sum_ - intersection + EPS)
+
+    if verbose:
+        print('Intersection:', intersection)
+        print('Union:', sum_ - intersection)
 
     return jac
 
@@ -65,8 +100,11 @@ class DeblendingScore(BaseScoreType):
         self.precision = precision
 
     def __call__(self, y_true, y_pred):
-        iou_score = iou(y_true, y_pred)
-        return np.mean(iou_score)
+        iou_list = [iou_bitmap(yt, yp)
+                    for (yt, yp) in zip(y_true, y_pred)]
+        iou = np.mean(iou_list)
+
+        return iou
 
 
 score_types = [
@@ -158,7 +196,12 @@ def save_y_pred(y_pred, data_path, output_path, suffix):
     """
     y_pred_f_name = os.path.join(output_path, 'y_pred_{}'.format(suffix))
 
-    if y_pred.ndim == 3:
+    if y_pred.ndim > 2:
         y_pred = y_pred.reshape(len(y_pred), -1)
 
-    np.savez_compressed(y_pred_f_name, y_pred=y_pred.astype(bool))
+    # Make sure array is rounded
+    y_pred = y_pred.round()
+    # Convert to boolean to save space and enble high compression
+    y_pred = y_pred.astype(bool)
+
+    np.savez_compressed(y_pred_f_name, y_pred=y_pred)
